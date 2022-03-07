8b2e675dbeef3afa203a9b4a92eb7cd3d0be1564
kien220201 <92934951+kien220201@users.noreply.github.com>
2022-03-08 02:12:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Bé Cúc đáng yêu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng ngày 8-3';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc heo quay của anh 8-3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Hãy luôn vui vẻ, tự tin, năng động và giỏi giang như vậy nhé!! Yêu em';
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
